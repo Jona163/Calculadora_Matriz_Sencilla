9c516bde6141d7f9032261e93dffbff5cd5504ca
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-16 07:07:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 # Fecha: 16 octubre 2024
 # Descripción: Calculadora matrix Python.
 # GitHub: https://github.com/Jona163
+import numpy as np
+import subprocess
+
+def OKI(n):
+    try:
+        n=int(n)
+    except:
+        n=OKI(input("Caracter no valido: "))
+    return n
+
+def OK(n):
+    try:
+        n=float(n)
+    except:
+        n=OK(input("Caracter no valido: "))
+    return n
